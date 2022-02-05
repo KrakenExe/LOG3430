f9bccd1da218124f304c50531aa9348ce71bc719
allen <allen1999@hotmail.ca>
2022-02-04 21:33:59 -0500

    update run.sh

--- TP1 code/run.sh
+++ TP1 code/run.sh
@@ -1,14 +1,4 @@
-python -m coverage run -m unittest test_email_analyzer.py
+python -m coverage run -m unittest test_vocabulary_creator.py test_crud.py test_email_analyzer.py
 python -m coverage report
-python -m coverage run -m --source=. --branch unittest test_email_analyzer.py
+python -m coverage run -m --source=. --branch unittest test_vocabulary_creator.py test_crud.py test_email_analyzer.py
 python -m coverage report
-
-python -m coverage run -m unittest test_vocabulary_creator.py
-python -m coverage report
-python -m coverage run -m --source=. --branch unittest test_vocabulary_creator.py
-python -m coverage report 
-
-python -m coverage run -m unittest test_crud.py
-python -m coverage report
-python -m coverage run -m --source=. --branch unittest test_crud.py
-python -m coverage report 
