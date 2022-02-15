0fcb1f8847bb5bdfb23ea57731055ef2d8107d53
Raphael Lasalle <raphael.lasalle@polymtl.ca>
2022-02-15 00:13:32 -0500

    refactor et complétion de la fonction CACC

--- TP1 code/criteres.py
+++ TP1 code/criteres.py
@@ -23,28 +23,59 @@ def spam_classification_2(is_spam,time_between_first_and_last,user_trust,group_t
     return S
 
 def CACC():
-    n_entries = 16.0
+    N_MINOR_CLAUSE_COMBINATIONS = 8
+    N_CLAUSES = 4
     CACC_test_set = []
+    # valeurs qui mettent la variable associée à vrai / faux dans les fonctions spam_classification
+    H_TRUE_VALUE = 15
+    H_FALSE_VALUE = 25
+    U_TRUE_VALUE = 25
+    U_FALSE_VALUE = 75
+    G_TRUE_VALUE = 75
+    G_FALSE_VALUE = 25
+    P_VALUES = [False,True]
+    H_VALUES = [25,15]
+    U_VALUES = [75,25]
+    G_VALUES = [25,75]
 
-    # valeurs qui mettent la variable associée à vrai / faux
-    H_true_value = 15
-    H_false_value = 25
-    U_true_value = 25
-    U_false_value = 75
-    G_true_value = 75
-    G_false_value = 25
+    for i in range(N_CLAUSES):
 
-    # P en clause majeure
-    for i in range(8):
-        entry_as_string = str(format(i, '#05b'))[2:]
-        print(entry_as_string)
-        H = H_true_value if entry_as_string[0]=='1' else H_false_value
-        U = U_true_value if entry_as_string[1]=='1' else U_false_value
-        G = G_true_value if entry_as_string[2]=='1' else G_false_value
-        if spam_classification_1(True,H,U,G) != spam_classification_1(False,H,U,G):
-            CACC_test_set.append([True,H,U,G])
-            CACC_test_set.append([False,H,U,G])
-            break
-    print(CACC_test_set)    
-
+        for j in range(N_MINOR_CLAUSE_COMBINATIONS):
+            entry_as_binary = int(bin(j),2)
+            minor_clause_1 = (entry_as_binary >> 2) & 1
+            minor_clause_2 = (entry_as_binary >> 1) & 1
+            minor_clause_3 = (entry_as_binary >> 0) & 1
+            # si P est la clause majeure
+            if i==0:
+                major_clause_true = spam_classification_1(P_VALUES[1],H_VALUES[minor_clause_1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3])
+                major_clause_false = spam_classification_1(P_VALUES[0],H_VALUES[minor_clause_1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3])
+                if major_clause_true != major_clause_false:
+                    CACC_test_set.append([P_VALUES[1],H_VALUES[minor_clause_1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3]])
+                    CACC_test_set.append([P_VALUES[0],H_VALUES[minor_clause_1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3]])
+                    break
+            # si H est la clause majeure
+            elif i==1:
+                major_clause_true = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3])
+                major_clause_false = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[0],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3])
+                if major_clause_true != major_clause_false:
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3]])
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[0],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3]])
+                    break
+            # si U est la clause majeure
+            elif i==2:
+                major_clause_true = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[1],G_VALUES[minor_clause_3])
+                major_clause_false = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[0],G_VALUES[minor_clause_3])
+                if major_clause_true != major_clause_false:
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[1],G_VALUES[minor_clause_3]])
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[0],G_VALUES[minor_clause_3]])
+                    break
+            # si G est la clause majeure 
+            elif i==3:
+                major_clause_true = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[1])
+                major_clause_false = spam_classification_1(P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[0])
+                if major_clause_true != major_clause_false:
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[1]])
+                    CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[0]])
+    print(CACC_test_set)
+        
 CACC()
