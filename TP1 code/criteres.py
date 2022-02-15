5b55f0d98a86fa8131b924fd4029e1f2c513cb63
Raphael Lasalle <raphael.lasalle@polymtl.ca>
2022-02-15 09:55:35 -0500

    modifications mineures à CACC et affichage du jeu de test

--- TP1 code/criteres.py
+++ TP1 code/criteres.py
@@ -1,24 +1,21 @@
 from email_analyzer import EmailAnalyzer
 from vocabulary_creator import VocabularyCreator
 from renege import RENEGE
+import re
 
 
 
-def spam_classification_1(is_spam,time_between_first_and_last,user_trust,group_trust):
-    P = is_spam
-    H = True if time_between_first_and_last<20 else False
-    U = True if user_trust<50 else False
-    G = True if group_trust>=50 else False
-
+def spam_classification_1(P,H,U,G):
+    H = True if H<20 else False
+    U = True if U<50 else False
+    G = True if G>=50 else False
     S = P and ((H and U) or (U and not G))
     return S
 
-def spam_classification_2(is_spam,time_between_first_and_last,user_trust,group_trust):
-    P = is_spam
-    H = True if time_between_first_and_last<20 else False
-    U = True if user_trust<50 else False
-    G = True if group_trust>=50 else False
-
+def spam_classification_2(P,H,U,G):
+    H = True if H<20 else False
+    U = True if U<50 else False
+    G = True if G>=50 else False
     S = P and H and U or P and U and not G
     return S
 
@@ -26,25 +23,29 @@ def CACC():
     N_MINOR_CLAUSE_COMBINATIONS = 8
     N_CLAUSES = 4
     CACC_test_set = []
-    # valeurs qui mettent la variable associée à vrai / faux dans les fonctions spam_classification
-    H_TRUE_VALUE = 15
-    H_FALSE_VALUE = 25
-    U_TRUE_VALUE = 25
-    U_FALSE_VALUE = 75
-    G_TRUE_VALUE = 75
-    G_FALSE_VALUE = 25
+
+    # valeurs qui mettent la variable associée à vrai / faux dans les fonctions spam_classification (valeur à [0] = faux, [1] = vrai)
     P_VALUES = [False,True]
     H_VALUES = [25,15]
     U_VALUES = [75,25]
     G_VALUES = [25,75]
 
+    #index pour l'affichage du jeu de test
+    P_INDEX = 0
+    H_INDEX = 1
+    U_INDEX = 2
+    G_INDEX = 3
+
     for i in range(N_CLAUSES):
 
         for j in range(N_MINOR_CLAUSE_COMBINATIONS):
             entry_as_binary = int(bin(j),2)
-            minor_clause_1 = (entry_as_binary >> 2) & 1
+
+            # permet d'obtenir les valeurs des bits en 1ere, 2eme et 3eme position dans entry_as_binary, qui représentent les entrées des 3 clauses mineures
+            minor_clause_1 = (entry_as_binary >> 2) & 1 
             minor_clause_2 = (entry_as_binary >> 1) & 1
             minor_clause_3 = (entry_as_binary >> 0) & 1
+
             # si P est la clause majeure
             if i==0:
                 major_clause_true = spam_classification_1(P_VALUES[1],H_VALUES[minor_clause_1],U_VALUES[minor_clause_2],G_VALUES[minor_clause_3])
@@ -76,6 +77,12 @@ def CACC():
                 if major_clause_true != major_clause_false:
                     CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[1]])
                     CACC_test_set.append([P_VALUES[minor_clause_1],H_VALUES[minor_clause_2],U_VALUES[minor_clause_3],G_VALUES[0]])
-    print(CACC_test_set)
+                    break
+    print("jeu de test pour le critère CACC:\n")
+    for i in range(len(CACC_test_set)):
+        output = "d{index} = <(P={P}, H={H}, U={U}, G={G}),({result})>"
+        S = spam_classification_1(CACC_test_set[i][P_INDEX],CACC_test_set[i][H_INDEX],CACC_test_set[i][U_INDEX],CACC_test_set[i][G_INDEX])
+        print(output.format(index=i+1,P=CACC_test_set[i][P_INDEX],H=CACC_test_set[i][H_INDEX],U=CACC_test_set[i][U_INDEX],G=CACC_test_set[i][G_INDEX],result=S))
+      
         
 CACC()
